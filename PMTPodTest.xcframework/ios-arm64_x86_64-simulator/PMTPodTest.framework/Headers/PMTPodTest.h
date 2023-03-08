9a448a18873193446ebff58742ff97d5ef9d78f9
phucmt <thanhphuc.mai@n2nconnect.com>
2023-03-08 10:32:46 +0700

    first local commit

---
+++ PMTPodTest.xcframework/ios-arm64_x86_64-simulator/PMTPodTest.framework/Headers/PMTPodTest.h
@@ -0,0 +1,18 @@
+//
+//  PMTPodTest.h
+//  PMTPodTest
+//
+//  Created by PhucMT on 08/03/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PMTPodTest.
+FOUNDATION_EXPORT double PMTPodTestVersionNumber;
+
+//! Project version string for PMTPodTest.
+FOUNDATION_EXPORT const unsigned char PMTPodTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PMTPodTest/PublicHeader.h>
+#import <PMTPodTest/Hello.h>
+
---
+++ PMTPodTest/Hello.m
@@ -0,0 +1,16 @@
+//
+//  Hello.m
+//  PMTPodTest
+//
+//  Created by PhucMT on 08/03/2023.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+
+- (NSString *)hello {
+    return @"Hello, world!";
+}
+
+@end
